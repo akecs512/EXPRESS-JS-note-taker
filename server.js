435c0f70c93bea98e79442303a4b1d23170626e5
Adrienne Kecskes <akecs619@yahoo.com>
2023-08-06 15:03:46 -0500

    save notes to list

--- server.js
+++ server.js
@@ -1,41 +1,38 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const PORT = 3001;
 // app.use('/api/notes', (req, res, next) => {
-//     console.log(req.body); 
-//     next(); 
+//     console.log(req.body);
+//     next();
 // });
 
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/notes.html'));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
 });
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'db/db.json'));
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "db/db.json"));
 });
-app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    console.log(newNote);
-    res.json(newNote);
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  console.log(newNote);
+  const data = fs.readFileSync("db/db.json");
+  const notesList = JSON.parse(data);
+  notesList.push(newNote);
+  fs.writeFileSync("db/db.json", JSON.stringify(notesList));
+
+  res.sendStatus(200);
 });
 
-app
-.listen(PORT, () => {
-    console.log(
-        `Server listening to http://localhost:${PORT}`
-);
-})
-
-
- 
-
-
+app.listen(PORT, () => {
+  console.log(`Server listening to http://localhost:${PORT}`);
+});
