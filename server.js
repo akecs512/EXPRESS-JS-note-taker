898e6a138413caba850e32fd350f1af6c4492b83
Adrienne Kecskes <akecs619@yahoo.com>
2023-08-06 14:04:12 -0500

    post working

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const path = require('path');
 
 const app = express();
 const PORT = 3001;
+// app.use('/api/notes', (req, res, next) => {
+//     console.log(req.body); 
+//     next(); 
+// });
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -14,6 +18,14 @@ app.get('/', (req, res) => {
 });
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'db/db.json'));
+});
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    console.log(newNote);
+    res.json(newNote);
 });
 
 app
